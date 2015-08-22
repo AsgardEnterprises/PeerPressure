26465b88b88e7d88c2073e0230de4bb547481789
Kadi Kraman <kadi.kraman@red-badger.com>
2015-08-22 19:56:53 +0100

    Single quotes are your friends!

--- frontend/lib/scripts/login.jsx
+++ frontend/lib/scripts/login.jsx
@@ -2,17 +2,17 @@ var React = require('react');
 
 var LoginForm = React.createClass({
   render: function() {
-    return (<form id="login-form" action="http://phpoll.com/login/process" method="post" role="form">
+    return (<form id='login-form' action='http://peerpressure.herokuapp.com/auth/' method='post' role='form'>
               <div className={'form-group'}>
-                <input type="text" name="username" id="username" tabindex="1" className={'form-control'} placeholder="Full Name" value="" />
+                <input type='text' name='username' id='email' tabindex='1' className={'form-control'} placeholder='Full Name' value='' />
               </div>
               <div className={'form-group'}>
-                <input type="password" name="password" id="password" tabindex="2" className={'form-control'} placeholder="Password" />
+                <input type='password' name='password' id='password' tabindex='2' className={'form-control'} placeholder='Password' />
               </div>
               <div className={'form-group'}>
                 <div className={'row'}>
                   <div className={'col-sm-6 col-sm-offset-3'}>
-                    <input type="submit" name="login-submit" id="login-submit" tabindex="4" className={'form-control btn btn-submit'} value="Log In" />
+                    <input type='submit' name='login-submit' id='login-submit' tabindex='4' className={'form-control btn btn-submit'} value='Log In' />
                   </div>
                 </div>
               </div>
@@ -22,23 +22,23 @@ var LoginForm = React.createClass({
 
 var RegisterForm = React.createClass({
   render: function() {
-    return (<form id="register-form" action="http://phpoll.com/register/process" method="post" role="form">
+    return (<form id='register-form' action='http://peerpressure.herokuapp.com/users/' method='post' role='form'>
               <div className={'form-group'}>
-                <input type="text" name="username" id="username" tabindex="1" className={'form-control'} placeholder="Full Name" value="" />
+                <input type='text' name='fullname' id='fullname' tabindex='1' className={'form-control'} placeholder='Full name' value='' />
               </div>
               <div className={'form-group'}>
-                <input type="email" name="email" id="email" tabindex="1" className={'form-control'} placeholder="Email Address" value="" />
+                <input type='email' name='email' id='email' tabindex='1' className={'form-control'} placeholder='Email Address' value='' />
               </div>
               <div className={'form-group'}>
-                <input type="password" name="password" id="password" tabindex="2" className={'form-control'} placeholder="Password" />
+                <input type='password' name='password' id='password' tabindex='2' className={'form-control'} placeholder='Password' />
               </div>
               <div className={'form-group'}>
-                <input type="password" name="confirm-password" id="confirm-password" tabindex="2" className={'form-control'} placeholder="Confirm Password" />
+                <input type='password' name='confirm-password' id='confirm-password' tabindex='2' className={'form-control'} placeholder='Confirm Password' />
               </div>
               <div className={'form-group'}>
                 <div className={'row'}>
                   <div className={'col-sm-6 col-sm-offset-3'}>
-                    <input type="submit" name="register-submit" id="register-submit" tabindex="4" className={'form-control btn btn-submit'} value="Register Now" />
+                    <input type='submit' name='register-submit' id='register-submit' tabindex='4' className={'form-control btn btn-submit'} value='Register Now' />
                   </div>
                 </div>
               </div>
