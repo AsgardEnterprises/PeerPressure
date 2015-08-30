8c18f6170b8cd25c2a4132f8a983c23f80335b04
Kadi Kraman <kadi.kraman@red-badger.com>
2015-08-30 19:10:13 +0100

    Add default data to the User Details and Friends Details components

--- frontend/lib/scripts/index.jsx
+++ frontend/lib/scripts/index.jsx
@@ -1,14 +1,45 @@
 var React = require('react');
 
 var UserDetails = React.createClass({
+	getInitialState: function(){
+		return {
+			name: 'Kate',
+			task: {
+				name: 'do 50 pushups ',
+				done: true
+			}
+		}
+	},
+
 	render: function(){
-		return <div className={'test-div'}>User details go here</div>
+		return <div className={'panel panel-default'}>Hello {this.state.name}. Your task is to {this.state.task.name}
+		today and you have {this.state.task.done ? '' : 'not'} completed it!</div>
 	}
 })
 
 var FriendsDetails = React.createClass({
+	getInitialState: function(){
+		return {
+			friends: [
+				{
+					name: 'Kadi',
+					doneTask: false
+				},
+				{
+					name: 'Marcel',
+					doneTask: true
+				}
+			]
+		}
+	},
+
 	render: function(){
-		return <div className={'test-div'}>Friends details go here</div>
+		return (<div className={'panel panel-default'}>
+			{this.state.friends.forEach(function(friend){
+				console.log(friend)
+			})}
+			Hello
+		</div>)
 	}
 })
 
